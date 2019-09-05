7ff21887ab7782ccf8404da99e9b602a12565513
SProbsJU95 <robson.santos242@etec.sp.gov.br>
2019-09-04 21:51:54 -0300

    Initial commit

---
+++ app/src/main/java/br/com/etecia/teste_estilo/MainActivity.java
@@ -0,0 +1,28 @@
+package br.com.etecia.teste_estilo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button btnAbrirJanela02;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnAbrirJanela02 = (Button)findViewById(R.id.btnAbrirJanela02);
+        btnAbrirJanela02.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),JanelaDois_Activity.class));
+                finish();
+            }
+        });
+
+
+    }
+}
