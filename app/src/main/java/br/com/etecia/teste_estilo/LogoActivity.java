eb3f24bc51fa06cece07ce2d1ee448751aaacee6
mjigormj <igorcruzsilva17@gmail.com>
2019-09-10 22:16:17 -0300

    Não!

---
+++ app/src/main/java/br/com/etecia/teste_estilo/LogoActivity.java
@@ -0,0 +1,13 @@
+package br.com.etecia.teste_estilo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LogoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.logo_layout);
+    }
+}
---
+++ app/src/main/java/br/com/etecia/teste_estilo/terceiroActivity.java
@@ -0,0 +1,13 @@
+package br.com.etecia.teste_estilo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class terceiroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.terceiro_layout);
+    }
+}
