3a5d1d1e8ceca65675f7cc63f8e492acbd760c01
SProbsJU95 <robson.santos242@etec.sp.gov.br>
2019-10-30 21:51:20 -0300

    LOgin feito

---
+++ app/src/main/java/br/com/etecia/teste_estilo/App_login_cadastro_Activity.java
@@ -0,0 +1,13 @@
+package br.com.etecia.teste_estilo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class App_login_cadastro_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.app_login_cadastro_layout);
+    }
+}
