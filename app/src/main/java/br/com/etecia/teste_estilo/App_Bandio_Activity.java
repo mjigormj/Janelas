67b0f98d37b0bd23ca2e229d2e0cae5ec8f194ee
SProbsJU95 <robson.santos242@etec.sp.gov.br>
2019-10-30 22:20:49 -0300

    Bandio

---
+++ app/src/main/java/br/com/etecia/teste_estilo/App_Bandio_Activity.java
@@ -0,0 +1,13 @@
+package br.com.etecia.teste_estilo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class App_Bandio_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.app__bandio_layout);
+    }
+}
