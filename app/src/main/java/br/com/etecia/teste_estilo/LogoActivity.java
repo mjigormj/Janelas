5651d6288b3515df6b7f00f3babb0feb1b02202c
Davi de Oliveira <davi.santos123@etec.sp.gov.br>
2019-11-06 22:13:40 -0300

    Bandio

--- app/src/main/java/br/com/etecia/teste_estilo/LogoActivity.java
+++ app/src/main/java/br/com/etecia/teste_estilo/LogoActivity.java
@@ -1,5 +1,7 @@
 package br.com.etecia.teste_estilo;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,15 @@ public class LogoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.logo_layout);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(LogoActivity.this, EstrelaActivity.class);
+                startActivity(intent);
+                finish();
+            }
+
+        }, 4000);
     }
 }
